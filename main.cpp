2b1de140414d94d17072351ead93c9673226d6e8
Matheus H. Potrich <matheushp.com@hotmail.com>
2025-03-19 20:52:49 -0300

    added classes

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include <string>
+
+class Piece 
+{
+	private:
+		std::string color;
+		Boolean is_upgradable;
+
+	public:
+		Piece(std::string color, bool is_upgradable)
+		{
+			this->color = color;
+			this->is_upgradable = is_upgradable;
+		}
+
+		std::string get_color()
+		{
+			return this->color;
+		}
+
+		bool get_is_upgradable()
+		{
+			return this->is_upgradable;
+		}
+
+		void change_is_upgradable(bool new_value)
+		{
+			this->is_upgradable = new_value;
+		}
+}
+
+class Space
+{
+	private:
+		Piece piece;
+		Boolean is_avaliable = true;
+
+	public:
+		bool check_is_avaliable()
+		{
+			return is_avaliable;
+		}
+
+		void change_piece(Piece new_piece)
+		{
+			this->piece = new_piece;
+			this->is_avaliable = false;
+		}
+
+		void remove_piece()
+		{
+			this->piece = NULL;
+			this->is_avaliable = true;
+		}
+}
+
+int main()
+{
+	Space table[7][7];
+	return 0;
+}
